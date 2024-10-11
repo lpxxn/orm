70882271fc7f1c0d5c5533c164eafd1b47fc266a
lpxxn <mi_duo@live.com>
2024-10-11 15:06:31 +0800

    feat: wire

--- wire/advanced/structproviders/wire.go
+++ wire/advanced/structproviders/wire.go
@@ -5,8 +5,9 @@ package structproviders
 import "github.com/google/wire"
 
 func InitializeEvent(phrase string, code int) (Event, error) {
-	// `*` 表示通配符，不显式指定的字段不会被赋值
-	// wire.Build(NewEvent, NewGreeter, wire.Struct(new(Message), "*"))
+	// `*` 表示通配符，都赋值
+	//wire.Build(NewEvent, NewGreeter, wire.Struct(new(Message), "*"))
+	// 指定字段，只赋值指定字段
 	wire.Build(NewEvent, NewGreeter, wire.Struct(new(Message), "Content"))
 	return Event{}, nil
 }
